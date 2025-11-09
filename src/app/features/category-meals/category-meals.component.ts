00c98681ca0aad19fa1366fa749e67ddc800fef0
sherif <sherif_shamel15@yahoo.com>
2025-11-09 10:59:47 +0200

    added pagination in meals section

--- src/app/features/category-meals/category-meals.component.ts
+++ src/app/features/category-meals/category-meals.component.ts
@@ -3,10 +3,11 @@ import { MealCategoriesService } from '../../shared/services/meal-categories.ser
 import { ActivatedRoute } from '@angular/router';
 import { Variables } from '../../variables/variables';
 import { IMeals } from '../../core/interfaces/imeals.interface';
+import { NgxPaginationModule } from 'ngx-pagination'; // <-- import the module
 
 @Component({
   selector: 'app-category-meals',
-  imports: [],
+  imports: [NgxPaginationModule],
   templateUrl: './category-meals.component.html',
   styleUrl: './category-meals.component.css',
 })
@@ -18,6 +19,11 @@ export class CategoryMealsComponent implements OnInit {
 
   mealCategory!: string | null;
   meals: IMeals[] = [];
+  // for pagination
+  p: number = 1;
+  pageSize: number = 12;
+  total: number = 0;
+
   // pageTitle: any = Variables.title;
   ngOnInit(): void {
     this._ActivatedRoute.paramMap.subscribe({
@@ -33,11 +39,20 @@ export class CategoryMealsComponent implements OnInit {
     this._MealCategoriesService.getMeals(this.mealCategory).subscribe({
       next: (res) => {
         this.meals = res.meals;
+        this.total = res.meals.length;
         console.log(this.meals);
       },
       error: (err) => {
         console.log(err);
       },
+    });
+  }
+  pageChanged(event: any) {
+    this.p = event
+   
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
     });
   }
 }
